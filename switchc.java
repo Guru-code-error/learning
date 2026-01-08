09473af500659e06c4428609de53556e83b3680b
subramani s <kgfguru2003@gmail.com>
2026-01-08 08:27:40 +0530

    Create switchc.java

---
+++ switchc.java
@@ -0,0 +1,23 @@
+public class switchc{
+    public static void main(String[] arg){
+        int a=10;
+        switch(a)
+        {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 10:
+                System.out.println("ten");
+                break;
+            default:
+                System.out.println("nothing");
+        }
+        System.out.println("switch will be end!!!");
+    }
+}
